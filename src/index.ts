d487882cc99817704ee13a05a7c9c29d5e04afdf
TomPenguin <tom.penguin.zoo@gmail.com>
2023-08-31 12:08:39 +0900

    Add util

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./crawler.js";
 export * as serializer from "./serializer/index.js";
+export * from "./util.js";
---
+++ src/util.ts
@@ -0,0 +1,15 @@
+import { h1 } from "md-utils-ts";
+import { Page, Pages } from "./crawler.js";
+
+const nestHeading = (text: string) => (text.match(/^#+\s/) ? "#" + text : text);
+
+export const pageToString = ({ lines, metadata }: Page): string => {
+  const title = h1(metadata.title);
+  const body = lines.map(nestHeading);
+  return [title, ...body].join("\n");
+};
+
+export const pagesToString = (pages: Pages): Record<string, string> =>
+  Object.fromEntries(
+    Object.entries(pages).map(([pageId, page]) => [pageId, pageToString(page)]),
+  );
