7b9d2ae5c3052efa09dce0df20074d628a7bb7fc
TomPenguin <tom.penguin.zoo@gmail.com>
2023-09-01 17:03:25 +0900

    Add some property functions

--- src/serializer/property/defaults.ts
+++ src/serializer/property/defaults.ts
@@ -3,40 +3,79 @@ import { has } from "../../libs.js";
 import { fromDate, fromRichText, fromUser } from "../utils.js";
 import { Serializer } from "./types.js";
 
-export const DELIMITER = ": ";
+export const DELIMITER = ", ";
 
 type Checkbox = Serializer<"checkbox">;
-export const checkbox: Checkbox = (name, property) =>
-  `[${name}] ${property.checkbox}`;
+export const checkbox: Checkbox = (name, prop) => `[${name}] ${prop.checkbox}`;
 
 type CreatedBy = Serializer<"created_by">;
-export const createdBy: CreatedBy = (name, property) =>
-  `[${name}] ${fromUser(property.created_by)}`;
+export const createdBy: CreatedBy = (name, prop) =>
+  `[${name}] ${fromUser(prop.created_by)}`;
 
 type CreatedTime = Serializer<"created_time">;
-export const createdTime: CreatedTime = (name, property) =>
-  `[${name}] ${property.created_time}`;
+export const createdTime: CreatedTime = (name, prop) =>
+  `[${name}] ${prop.created_time}`;
 
 type _Date = Serializer<"date">;
-export const date: _Date = (name, property) =>
-  `[${name}] ${fromDate(property.date)}`;
+export const date: _Date = (name, prop) => `[${name}] ${fromDate(prop.date)}`;
 
 type Email = Serializer<"email">;
-export const email: Email = (name, property) => `[${name}] ${property.email}`;
+export const email: Email = (name, prop) => `[${name}] ${prop.email}`;
 
 type Title = Serializer<"title">;
-export const title: Title = (name, property) =>
-  `[${name}] ${fromRichText(property.title)}`;
+export const title: Title = (name, prop) =>
+  `[${name}] ${fromRichText(prop.title)}`;
 
 type Files = Serializer<"files">;
-export const files: Files = (name, property) =>
-  `[${name}]` +
-  property.files
+export const files: Files = (name, prop) =>
+  `[${name}] ` +
+  prop.files
     .map((file) => {
       const href = has(file, "external") ? file.external.url : file.file.url;
       return anchor(file.name, href);
     })
-    .join(", ");
+    .join(DELIMITER);
+
+type Formula = Serializer<"formula">;
+export const formula: Formula = (name, prop) => {
+  switch (prop.formula.type) {
+    case "string":
+      return `[${name}] ${prop.formula.string}`;
+    case "boolean":
+      return `[${name}] ${prop.formula.boolean}`;
+    case "date":
+      return `[${name}] ${fromDate(prop.formula.date)}`;
+    case "number":
+      return `[${name}] ${prop.formula.number}`;
+  }
+};
+
+type LastEditedBy = Serializer<"last_edited_by">;
+export const lastEditedBy: LastEditedBy = (name, prop) =>
+  `[${name}] ${fromUser(prop.last_edited_by)}`;
+
+type LastEditedTime = Serializer<"last_edited_time">;
+export const lastEditedTime: LastEditedTime = (name, prop) =>
+  `[${name}] ${prop.last_edited_time}`;
+
+type MultiSelect = Serializer<"multi_select">;
+export const multiSelect: MultiSelect = (name, prop) =>
+  `[${name}] ` + prop.multi_select.map((select) => select.name).join(DELIMITER);
+
+type _Number = Serializer<"number">;
+export const number: _Number = (name, prop) => `[${name}] ${prop.number}`;
+
+type People = Serializer<"people">;
+export const people: People = (name, prop) =>
+  `[${name}] ` + prop.people.map((person) => fromUser(person)).join(DELIMITER);
+
+type PhoneNumber = Serializer<"phone_number">;
+export const phoneNumber: PhoneNumber = (name, prop) =>
+  `[${name}] ${prop.phone_number}`;
+
+type Relation = Serializer<"relation">;
+export const relation: Relation = (name, prop) =>
+  `[${name}] ` + prop.relation.map((item) => `${item.id}`).join(DELIMITER);
 
 export const defaults = {
   title,
@@ -46,4 +85,12 @@ export const defaults = {
   date,
   email,
   files,
+  formula,
+  lastEditedBy,
+  lastEditedTime,
+  multiSelect,
+  number,
+  people,
+  phoneNumber,
+  relation,
 };
