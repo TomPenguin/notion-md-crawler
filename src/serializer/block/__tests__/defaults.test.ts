b449f4e298d722822ee4687e36ac8b1ced5ac226
TomPenguin <tom.penguin.zoo@gmail.com>
2023-09-04 08:20:30 +0900

    add test

--- src/serializer/block/__tests__/defaults.test.ts
+++ src/serializer/block/__tests__/defaults.test.ts
@@ -1,5 +1,5 @@
-import { fromLink } from "../../utils.js";
-import { audio } from "../defaults.js";
+import { fromLink, fromRichText } from "../../utils.js";
+import { audio, bookmark, bulletedListItem } from "../defaults.js";
 
 describe("audio", () => {
   it("should return a markdown anchor tag when a valid block object is provided", () => {
@@ -29,5 +29,102 @@ describe("audio", () => {
     const { title, href } = fromLink(block.audio as any);
     const expected = `[${title}](${href})`;
     expect(result).toBe(expected);
+  });
+});
+
+describe("bookmark", () => {
+  it("should correctly serialize a block with both bookmark caption and url", () => {
+    const block = {
+      bookmark: {
+        caption: [{ plain_text: "Google", annotations: {}, href: undefined }],
+        url: "https://www.google.com",
+      },
+    };
+    expect(bookmark(block as any)).toBe(
+      `[${fromRichText(
+        block.bookmark.caption as any,
+      )}](https://www.google.com)`,
+    );
+  });
+
+  it("should correctly serialize a block with no caption but with url", () => {
+    const block = {
+      bookmark: {
+        caption: [],
+        url: "https://www.google.com",
+      },
+    };
+    expect(bookmark(block as any)).toBe(`[](https://www.google.com)`);
+  });
+
+  it("should correctly serialize a block with a caption but no url", () => {
+    const block = {
+      bookmark: {
+        caption: [{ plain_text: "Google", annotations: {}, href: undefined }],
+        url: "",
+      },
+    };
+    expect(bookmark(block as any)).toBe(`[Google]()`);
+  });
+
+  it("should correctly serialize a block with neither caption nor url", () => {
+    const block = {
+      bookmark: {
+        caption: [],
+        url: "",
+      },
+    };
+    expect(bookmark(block as any)).toBe(`[]()`);
+  });
+
+  it("should correctly serialize a block with annotated caption and url", () => {
+    const block = {
+      bookmark: {
+        caption: [
+          {
+            plain_text: "Google",
+            annotations: { bold: true },
+            href: undefined,
+          },
+        ],
+        url: "https://www.google.com",
+      },
+    };
+    expect(bookmark(block as any)).toBe(`[**Google**](https://www.google.com)`);
+  });
+});
+
+describe("bulletedListItem", () => {
+  it("should prefix the rich text with a bullet symbol", () => {
+    const block = {
+      bulleted_list_item: {
+        rich_text: [
+          { plain_text: "Hello, World!", annotations: {}, href: undefined },
+        ],
+      },
+    };
+    expect(bulletedListItem(block as any)).toBe("- Hello, World!"); // Assuming that md.bullet uses '-' for bullets
+  });
+
+  it("should handle empty list items", () => {
+    const block = { bulleted_list_item: { rich_text: [] } };
+    expect(bulletedListItem(block as any)).toBe("- "); // Assuming that an empty bullet should still be output
+  });
+
+  it("should preserve formatting and annotations of the text", () => {
+    const block = {
+      bulleted_list_item: {
+        rich_text: [
+          {
+            plain_text: "Hello, World!",
+            annotations: { bold: true },
+            href: "https://example.com",
+          },
+        ],
+      },
+    };
+    expect(bulletedListItem(block as any)).toBe(
+      "- [**Hello, World!**](https://example.com)",
+    );
   });
 });
